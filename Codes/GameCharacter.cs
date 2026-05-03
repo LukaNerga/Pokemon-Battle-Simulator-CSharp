fb0b4b3fcd5a311e12fd47ce975c5b56b180050b
gorgodzepapuna123 <gorgodzepapuna@gmail.com>
2026-05-04 01:06:16 +0400

    Create GameCharacter.cs

---
+++ Codes/GameCharacter.cs
@@ -0,0 +1,16 @@
+namespace PokemonBattleSimulatorGUI
+{
+    // Base class for all characters in the game
+    public class GameCharacter
+    {
+        public string Name { get; set; } = "";
+        public int MaxHP { get; set; }
+        public int CurrentHP { get; set; }
+
+        // Virtual method (can be overridden)
+        public virtual string GetInfo()
+        {
+            return "Character: " + Name;
+        }
+    }
+}
