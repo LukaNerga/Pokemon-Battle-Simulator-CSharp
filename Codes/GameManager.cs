cff1bc73b37f0a0685cd975443876dcf788d1ebf
gorgodzepapuna123 <gorgodzepapuna@gmail.com>
2026-05-04 22:03:21 +0400

    Update GameManager.cs

--- Codes/GameManager.cs
+++ Codes/GameManager.cs
@@ -1,8 +1,8 @@
+
 namespace PokemonBattleSimulatorGUI
 {
     public static class GameManager
     {
-        // Stores current game progress.
         public static Pokemon? PlayerPokemon { get; set; }
         public static int CurrentLevel { get; set; } = 1;
 
@@ -12,22 +12,33 @@ namespace PokemonBattleSimulatorGUI
             CurrentLevel = 1;
         }
 
+        private static Random random = new Random();
+
         public static Pokemon GetEnemyForLevel(int level)
         {
-            // Enemy gets stronger by level.
-            if (level == 1) return new Pokemon("Vulpix", 90, 16, "Fire");
-            if (level == 2) return new Pokemon("Psyduck", 100, 18, "Water");
-            if (level == 3) return new Pokemon("Oddish", 110, 19, "Grass");
+            List<Pokemon> enemies = new List<Pokemon>();
 
-            return new Pokemon("Pikachu Boss", 130, 24, "Electric");
+            enemies.Add(new Pokemon("Vulpix", 90, 16, "Fire"));
+            enemies.Add(new Pokemon("Psyduck", 100, 18, "Water"));
+            enemies.Add(new Pokemon("Oddish", 110, 19, "Grass"));
+            enemies.Add(new Pokemon("Pidgey", 95, 17, "Flying"));
+            enemies.Add(new Pokemon("Geodude", 120, 16, "Rock"));
+            enemies.Add(new Pokemon("Machop", 115, 20, "Fighting"));
+
+            if (level == 4)
+            {
+                return new Pokemon("Pikachu Boss", 130, 24, "Electric");
+            }
+
+            int index = random.Next(enemies.Count);
+            return enemies[index];
         }
 
         public static string GetLevelName(int level)
         {
-            if (level == 1) return "Level 1 - Fire Opponent";
-            if (level == 2) return "Level 2 - Water Opponent";
-            if (level == 3) return "Level 3 - Grass Opponent";
-
+            if (level == 1) return "Level 1";
+            if (level == 2) return "Level 2";
+            if (level == 3) return "Level 3";
             return "Final Boss - Pikachu";
         }
     }
